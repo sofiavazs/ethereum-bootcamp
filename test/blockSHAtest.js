72101377f92a670130df543a8a3f573191ea6193
Sofia Sousa <sofiavazsousa@gmail.com>
2023-04-02 19:02:57 +0200

    Add week1: what’s in a hash exercise

---
+++ test/blockSHAtest.js
@@ -0,0 +1,17 @@
+const Block = require("../Week1-Blockchain Cryptography/4-Blockchain Network/2-What's in a hash/src/block");
+const assert = require("assert");
+const SHA256 = require("crypto-js/sha256");
+
+describe("Block", function () {
+  it("should store a random name", function () {
+    const randomName = require("faker").name.findName();
+    assert.equal(randomName, new Block(randomName).data);
+  });
+
+  it("should hash some random data", function () {
+    const randomEmail = require("faker").internet.email();
+    const myHash = SHA256(randomEmail).toString();
+    const yourHash = new Block(randomEmail).toHash().toString();
+    assert.equal(myHash, yourHash);
+  });
+});
